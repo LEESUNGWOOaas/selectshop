a2c56e668cacbad91e32a62d56c0b34192df3684
LEESUNGWOOaas <sa775628@gmail.com>
2021-04-25 16:35:09 +0900

    aws에 RDS(MySQL)와 Spring 연동 및 오류수정

--- src/main/java/com/example/selectshop/service/ProductService.java
+++ src/main/java/com/example/selectshop/service/ProductService.java
@@ -12,29 +12,29 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 
 
-@RequiredArgsConstructor
-@Service
+@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성
+@Service // 서비스 선언
 public class ProductService {
     //업데이트 메소드 필요 관심가격 정보가 전달됬다면 id 를 받아 해당상품을 찾고  myprice를 업데이트 해줘야한다
     private final ProductRepository productRepository;
 
-    @Transactional // DB정보가 업데이틑 되는데 반드시 필요한 어노테이션
+    @Transactional // 메소드 동작이 SQL 쿼리문임을 선언
     public Long update(Long id, ProductMyPriceRequestDto requestDto) {
         //id를 product를 찾고 결과로 product가 나온다
         Product product = productRepository.findById(id).orElseThrow(
-                () -> new NullPointerException("아이디가 존재하지 않습니다.")
+                () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
         );
         //그 정보를 update한다
         product.update(requestDto);
         return id;
     }
-    @Transactional
-    public Long updateBySearch(Long id, ItemDto itemDto){
-      Product product = productRepository.findById(id).orElseThrow(
-                ()-> new IllegalArgumentException("아이디가 존재하지 않습니다.")//에러는 상관이없다.
+
+    @Transactional // 메소드 동작이 SQL 쿼리문 선언
+    public Long updateBySearch(Long id, ItemDto itemDto) {
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("해당 아이디가 존재하지 않습니다.") //에러의 종류는 상관이없다.
         );
-      product.updateByItemDto(itemDto);
-      return id;
+        product.updateByItemDto(itemDto);
+        return id;
     }
 }
-
--- src/main/resources/static/basic.js
+++ src/main/resources/static/basic.js
@@ -1,7 +1,7 @@
 let targetId;
 
 $(document).ready(function () {
-    // id 가 query 인 객체 위에서 엔터를 누르면 execSearch() 함수를 실행해라
+    // id 가 query 인 객체 위에서 엔터를 누르면 execSearch() 함수를 실행하라는 뜻
     $('#query').on('keypress', function (e) {
         if (e.key == 'Enter') {
             execSearch();
@@ -33,65 +33,64 @@ $(document).ready(function () {
 })
 
 
-function numberWithCommas (x) { // 숫자를 넣어주면 반점(,)을 넣어주는 함수
+function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-/////
 
-function execSearch() { //검색을 실행하는 부분
+function execSearch() {
     /**
      * 검색어 input id: query
      * 검색결과 목록: #search-result-box
      * 검색결과 HTML 만드는 함수: addHTML
      */
-    $('#search-result-box').empty();// 검색했던 결과를 초기화한다.
-    // 1. 검색창의 입력값을 가져온다.
-    let query = $('#query').val();//.val()은 양식(form)의 값을 가져오거나 값을 설정하는 메소드
+        // 1. 검색창의 입력값을 가져온다.
+    let query = $('#query').val();
 
     // 2. 검색창 입력값을 검사하고, 입력하지 않았을 경우 focus.
-    if(query==''){
-         alert("검색어를 입력해주세요.");
-         $('#query').focus // focus는 검색창에 타자커서가 반짝이는 기능
+    if (query == '') {
+        alert('검색어를 입력해주세요');
+        $('#query').focus();
+        return;
     }
     // 3. GET /api/search?query=${query} 요청
-        $.ajax({
-            type : 'GET',
-            url : `/api/search?query=${query}`,
-            success : function(response){
-                // 4. for 문마다 itemDto를 꺼내서 HTML 만들고 검색결과 목록에 붙이기
-                for(let i=0;i<response.length;i++){
-                    let itemDto = response[i]; //itemDto값안에 하나하나 검색결과가 들어감
-                    let tempHtml = addHTML(itemDto);
-                    $('#search-result-box').append(tempHtml);
-                }
+    $.ajax({
+        type: 'GET',
+        url: `/api/search?query=${query}`,
+        success: function (response) {
+            $('#search-result-box').empty();
+            // 4. for 문마다 itemDto를 꺼내서 HTML 만들고 검색결과 목록에 붙이기
+            for (let i = 0; i < response.length; i++) {
+                let itemDto = response[i];//itemDto값안에 하나하나 검색결과가 들어감
+                let tempHtml = addHTML(itemDto);
+                $('#search-result-box').append(tempHtml);
             }
-
-        })
+        }
+    })
 
 }
 
-function addHTML(itemDto) { //itemDto 안에있는 데이터를 HTML로 바꿔주는 역할
+function addHTML(itemDto) {   //itemDto 안에있는 데이터를 HTML로 바꿔주는 역할
     /**
      * class="search-itemDto" 인 객체에서
      * image, title, lprice, addProduct 활용하기
-     * 참고) onclick='addProduct(${JSON.stringify(itemDto)})' // addproduct는 문자열의 형태로 받게된다.
+     * 참고) onclick='addProduct(${JSON.stringify(itemDto)})'  // addproduct는 문자열의 형태로 받게된다.
      */
     return `<div class="search-itemDto">
-            <div class="search-itemDto-left">
-                <img src="${itemDto.image}" alt="">
+        <div class="search-itemDto-left">
+            <img src="${itemDto.image}" alt="">
+        </div>
+        <div class="search-itemDto-center">
+            <div>${itemDto.title}</div>
+            <div class="price">
+                ${numberWithCommas(itemDto.lprice)}
+                <span class="unit">원</span>
             </div>
-            <div class="search-itemDto-center">
-                <div>${itemDto.title}</div>
-                <div class="price">
-                    ${numberWithCommas(itemDto.lprice)}
-                    <span class="unit">원</span>
-                </div>
-            </div>
-            <div class="search-itemDto-right">
-                <img src="images/icon-save.png" alt="" onclick='addProduct(${JSON.stringify(itemDto)})'>
-            </div>
-        </div>`
+        </div>
+        <div class="search-itemDto-right">
+            <img src="images/icon-save.png" alt="" onclick='addProduct(${JSON.stringify(itemDto)})'>
+        </div>
+    </div>`
 }
 
 function addProduct(itemDto) {
@@ -101,21 +100,18 @@ function addProduct(itemDto) {
      * 1. contentType: "application/json",
      * 2. data: JSON.stringify(itemDto),
      */
-
     // 1. POST /api/products 에 관심 상품 생성 요청
     $.ajax({
-        type : 'POST',
-        url : "/api/products",
-        data : JSON.stringify(itemDto),
-        contentType : "application/json",
-        // 2. 응답 함수에서 modal을 뜨게 하고, targetId 를 reponse.id 로 설정 (myprice 설정하기 위함)
-        success : function(response){
+        type: "POST",
+        url: '/api/products',
+        contentType: "application/json",
+        data: JSON.stringify(itemDto),
+        success: function (response) {
+            // 2. 응답 함수에서 modal을 뜨게 하고, targetId 를 reponse.id 로 설정
             $('#container').addClass('active');
-            targetId = response.id; //targetId = 전역변수 (제일최근에 담긴 관심상품 id가 담김)
+            targetId = response.id;
         }
     })
-
-
 }
 
 function showProduct() {
@@ -124,79 +120,73 @@ function showProduct() {
      * 검색결과 목록: #search-result-box
      * 관심상품 HTML 만드는 함수: addProductItem
      */
+
     // 1. GET /api/products 요청
-        $.ajx({
-            type : "GET",
-            url : '/api/products',
-            success : function(response){
-     // 2. 관심상품 목록, 검색결과 목록 비우기
-         $('#product-container').empty();
-         $('search-result-box').empty();
-     // 3. for 문마다 관심 상품 HTML 만들어서 관심상품 목록에 붙이기
-                for(let i=0;i<response.length;i++){
-                    let product = response[i];
-                    let tempHtml = addProduct(product);
-                    $('product-container').append(tempHtml);
-
-                }
+    $.ajax({
+        type: 'GET',
+        url: '/api/products',
+        success: function (response) {
+            // 2. 관심상품 목록, 검색결과 목록 비우기
+            $('#product-container').empty();
+            $('#search-result-box').empty();
+            // 3. for 문마다 관심 상품 HTML 만들어서 관심상품 목록에 붙이기!
+            for (let i = 0; i < response.length; i++) {
+                let product = response[i];
+                let tempHtml = addProductItem(product);
+                $('#product-container').append(tempHtml);
             }
-        })
-
-
-
-
+        }
+    })
 }
 
 function addProductItem(product) {
-    // link(네이버 쇼핑 페이지로 가기위한 링크), image, title, lprice, myprice 변수 활용
-
-    return ` <div class="product-card" onclick="window.location.href='${product.link}'">
-            <div class="card-header">
-                <img src="${product.image}" 
-                     alt="">
-            </div>
-            <div class="card-body">
-                <div class="title">
-                    ${product.title}
+    // link, image, title, lprice, myprice 변수 활용하기
+    return `<div class="product-card" onclick="window.location.href='${product.link}'">
+                <div class="card-header">
+                    <img src="${product.image}"
+                         alt="">
                 </div>
-                <div class="lprice">
-                    <span>${numberWithCommas(product.lprice)}</span>원
+                <div class="card-body">
+                    <div class="title">
+                        ${product.title}
+                    </div>
+                    <div class="lprice">
+                        <span>${numberWithCommas(product.lprice)}</span>원
+                    </div>
+                    <div class="isgood ${product.lprice > product.myprice ? 'none' : ''}">
+                        최저가
+                    </div>
                 </div>
-               
-                <div class="isgood ${product.lprice <= product.myprice ? '' : 'none'}"> 
-                    최저가
-                </div>
-            </div>
-        </div>`;
+            </div>`;
 }
 
 function setMyprice() {
 
-     // 1. id가 myprice 인 input 태그에서 값을 가져온다.
-        let myprice = $('myprice').val
+    // 1. id가 myprice 인 input 태그에서 값을 가져온다.
+    let myprice = $('myprice').val
 
-     // 2. 만약 값을 입력하지 않았으면 alert를 띄우고 중단한다.
-        if(myprice == ''){
-            alert('올바른 형식이 아닙니다. 설정가격을 넣어주세요')
+    // 2. 만약 값을 입력하지 않았으면 alert를 띄우고 중단한다.
+    if(myprice == ''){
+        alert('올바른 형식이 아닙니다. 설정가격을 넣어주세요')
+    }
+
+    // 3. PUT /api/product/${targetId} 에 data를 전달한다.
+    // 주의) contentType: "application/json",
+    //      data: JSON.stringify({myprice: myprice}),
+    //      빠뜨리지 말 것!
+    $.ajax({
+        type : "PUT",
+        url : '/api/products/{targetId}',
+        contentType : "application/json",
+        data : JSON.stringify({myprice:myprice}),
+        success : function (response){
+            //   4. 모달을 종료한다. $('#container').removeClass('active');
+            $('#container').removeClass('active');
+            //   5. 성공적으로 등록되었음을 알리는 alert를 띄운다.
+            alert('설정이 완료됬습니다.');
+            //   6. 창을 새로고침한다. window.location.reload();
+            window.location.reload();
         }
-
-     // 3. PUT /api/product/${targetId} 에 data를 전달한다.
-     // 주의) contentType: "application/json",
-     //      data: JSON.stringify({myprice: myprice}),
-     //      빠뜨리지 말 것!
-        $.ajax({
-            type : "PUT",
-            url : '/api/products/{targetId}',
-            contentType : "application/json",
-            data : JSON.stringify({myprice:myprice}),
-            success : function (response){
-      //   4. 모달을 종료한다. $('#container').removeClass('active');
-                $('#container').removeClass('active');
-      //   5. 성공적으로 등록되었음을 알리는 alert를 띄운다.
-                alert('설정이 완료됬습니다.');
-      //   6. 창을 새로고침한다. window.location.reload();
-                window.location.reload();
-            }
-        })
+    })
 
 }
